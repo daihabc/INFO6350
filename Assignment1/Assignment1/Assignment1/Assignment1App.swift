492c9ea05dbb7701a816a41f41aba4ae7908268f
X D <xd@Xs-MacBook-Pro.local>
2022-09-18 23:36:50 -0700

    adds assignment 1

---
+++ Assignment1/Assignment1/Assignment1/Assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1App.swift
+//  Assignment1
+//
+//  Created by X D on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
