c8cc51958580ec49c0b3eb7e663c4350748a7fff
daihabc <77610664+daihabc@users.noreply.github.com>
2022-10-01 14:04:35 -0700

    adds assignment2

---
+++ Assignment2/MyFavoriteFoods/MyFavoriteFoods/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  MyFavoriteFoods
+//
+//  Created by X D on 9/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+
+    var food = ["tacos", "shiwarma","dimsum"]
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tblView.delegate = self
+        tblView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return food.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = food[indexPath.row]
+        return cell
+    }
+
+}
+
